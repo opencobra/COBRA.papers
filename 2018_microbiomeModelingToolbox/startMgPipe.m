b5e76af103904d97002c4cdf04cf497d2ee61d91
almut.heinken <almut.heinken@gmail.com>
2020-10-13 14:37:42 +0100

    adapted mgPipe example driver file to recent changes in code

--- 2018_microbiomeModelingToolbox/startMgPipe.m
+++ 2018_microbiomeModelingToolbox/startMgPipe.m
@@ -6,43 +6,30 @@
 % Federico Baldini, 2017-2018
 
 initCobraToolbox()
-%REQUIRED INPUT VARIABLES
+
+%% REQUIRED INPUT VARIABLES
 
 % path to microbe models
-modPath='YOUR_PATH_TO_AGORA\';
-% path where to save results
-resPath='YOUR PATH TO RESULT FOLDER\';
-% path to where the COBRA Toolbox is located
-global CBTDIR
-toolboxPath=CBTDIR;
-% path to and name of the file with dietary information.
-dietFilePath=[CBTDIR filesep 'papers' filesep '2018_microbiomeModelingToolbox' filesep 'resources' filesep 'AverageEuropeanDiet'];
+system('curl -LJO https://github.com/VirtualMetabolicHuman/AGORA/archive/master.zip')
+unzip('AGORA-master')
+modPath = [pwd filesep 'AGORA-master' filesep 'CurrentVersion' filesep 'AGORA_1_03' filesep' 'AGORA_1_03_mat'];
+
 % path to and name of the file with abundance information.
-abunFilePath=[CBTDIR filesep 'papers' filesep '2018_microbiomeModelingToolbox' filesep 'examples' filesep 'normCoverage.csv'];
-% path to csv file for stratification criteria (if empty or not existent no criteria is used)
-indInfoFilePath='none'
-% name of objective function of organisms 
-objre={'EX_biomass(e)'};
-%the output is vectorized picture, change to '-dpng' for .png
-figForm = '-depsc'
+abunFilePath='normCoverage.csv';
+
+%% OPTIONAL INPUT VARIABLES
+
+% path where to save results
+mkdir('MicrobiomeModels')
+resPath=[pwd filesep 'MicrobiomeModels'];
+
+% path to and name of the file with dietary information.
+dietFilePath='AverageEuropeanDiet';
+
 % number of cores dedicated for parallelization 
 numWorkers = 3;
-% autofix for names mismatch
-autoFix = true;
-% if outputs in open formats should be produced for each section 
-compMod = false; 
-% to enable also rich diet simulations 
-rDiet =false;
-% if if to use an external solver and save models with diet
-extSolve = false;
-% the type of FVA function to use to solve
-fvaType=true;
-% To tourn off the autorun to be able to manually execute each part of the pipeline.
-autorun=false;
-%END OF REQUIRED INPUT VARIABLES
 
-%%
-%PIPELINE LAUNCHER 
-[init,modPath,toolboxPath,resPath,dietFilePath,abunFilePath,indInfoFilePath,objre,figForm,numWorkers,autoFix,compMod,rDiet,extSolve,fvaType,autorun]= initMgPipe(modPath, toolboxPath, resPath, dietFilePath, abunFilePath,indInfoFilePath, objre, figForm, numWorkers, autoFix, compMod, rDiet,extSolve,fvaType,autorun);
 
+%% PIPELINE LAUNCHER 
+[init, netSecretionFluxes, netUptakeFluxes, Y] = initMgPipe(modPath, abunFilePath, 'resPath', resPath, 'dietFilePath', dietFilePath, 'numWorkers', numWorkers);
 
